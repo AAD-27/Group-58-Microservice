21b272aa557783595fe9cfcbcdb88c8e030e156a
veroni-d20 <veronishwetha@gmail.com>
2025-11-09 22:42:33 +0530

    code changes

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,8 +3,7 @@ import axios from "axios";
 
 @Injectable()
 export class AppService {
-  async getHello(): Promise<any> {
-    const data = await axios.get("http://nestjs-app-2:3001/");
-    return data;
+  getHello(): string {
+    return "Hello World!";
   }
 }
